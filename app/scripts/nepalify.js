392d48f32b0cd3fc1bffdc6a661dcde38daac64a
Suvash Thapaliya <suvash@gmail.com>
2013-01-21 10:41:43 +0100

    Move all logic to one file.

--- app/scripts/nepalify.js
+++ app/scripts/nepalify.js
@@ -116,6 +116,29 @@ var nepalify = {
     unicodify: function(character)
     {
         return nepalify.RomanToNepaliUnicodeChar(character, nepalify.unicodeRomanToNepaliMap);
+    },
+
+    initialize: function(){
+      $(".nepalify").keypress(function(event) {
+        // Check if its a textinput and the right class applied
+        var key = nepalify.unicodify(event.which);
+        // console.log(key);
+
+        var target = event.target;
+
+        var selectionStart = target.selectionStart;
+        var selectionEnd = target.selectionEnd;
+
+        if (target.setSelectionRange) {  //text fields
+
+          target.value =  target.value.substring(0, selectionStart) + key + target.value.substring(selectionEnd);
+          target.setSelectionRange(selectionStart + key.length,selectionStart + key.length);
+
+        }
+        event.preventDefault();
+      });
     }
 
 };
+
+nepalify.initialize();
