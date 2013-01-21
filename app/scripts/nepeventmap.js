7a5f6fe7034de08aa74f5f82bfdc370bbdb36d6c
Suvash Thapaliya <suvash@gmail.com>
2013-01-21 10:33:28 +0100

    Proper variable names Rename everything to Nepalify get rid of a lot of JSLint errors

--- app/scripts/nepeventmap.js
+++ app/scripts/nepeventmap.js
@@ -1,21 +1,21 @@
+'use strict';
+
 (function eventmap(){
 
   $(document).keypress(function(event) {
     // Check if its a textinput and the right class applied
-    var key = npiext.unicodify(event.which);
+    var key = nepalify.unicodify(event.which);
     // console.log(key);
 
     var target = event.target;
 
-    var selection_start = target.selectionStart;
-    var selection_end = target.selectionEnd;
+    var selectionStart = target.selectionStart;
+    var selectionEnd = target.selectionEnd;
 
     if (target.setSelectionRange) {  //text fields
 
-      target.value =  target.value.substring(0, selection_start)
-                      + key
-                      + target.value.substring(selection_end);
-    target.setSelectionRange(selection_start + key.length,selection_start + key.length);
+      target.value =  target.value.substring(0, selectionStart) + key + target.value.substring(selectionEnd);
+      target.setSelectionRange(selectionStart + key.length,selectionStart + key.length);
 
     }
     event.preventDefault();
