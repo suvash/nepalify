f214b4e487d8373ebb0cf9014666f48f56f22e2c
Ansu Singh <ansumsingh@gmail.com>
2021-02-21 19:16:52 +1100

    Added test for validating expected output.

--- test/layout.spec.js
+++ test/layout.spec.js
@@ -4,12 +4,20 @@ var layouts = require('nepalify').layouts;
 describe('layouts.romanized', () => {
   describe('formatKey()',() => {
     it('Should return corresponding Nepali character.', () => {
-      var input = ["+", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "=", ">", "?", "A", "B", "C",
+      const input = ["+", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "=", ">", "?", "A", "B", "C",
                    "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
                    "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a",  "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
                    "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"];
+                   
+      const expected = ["\u200C", "।", "्", "०", "१", "२", "३", "४", "५", "६", "७", "८", "९", "ङ", "\u200D",
+         "॥", "?", "आ", "भ", "च", "ध", "ै","ऊ", "घ", "अ", "ी","झ", "ख", "ळ", "ं","ण", "ओ", "फ", "ठ", "ृ",
+         "श", "थ", "ू","ँ","औ", "ढ", "ञ", "ऋ", "इ", "ॐ", "ए", "^", " ","ऽ", "ा","ब", "छ", "द", "े", "उ",  
+         "ग",  "ह",  "ि", "ज",  "क",  "ल",  "म",  "न",  "ो", "प",  "ट", "र", "स", "त", "ु", "व",  "ौ", "ड",
+           "य",  "ष",  "ई",  "ः", "ऐ",  "़"];
       
-      assert.strictEqual(layouts.romanized.formatKey("+"), "\u200C");
+       input.every((element, index, array) => {
+        assert.strictEqual(layouts.romanized.formatKey(element), expected[index]);
+      });
     });
   });
 });
