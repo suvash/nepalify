ae850f0f5b8e918c893aebe9a33622883011094d
Ansu Singh <ansumsingh@gmail.com>
2021-02-21 22:20:35 +1100

    Some formating

--- test/layout.spec.js
+++ test/layout.spec.js
@@ -1,40 +1,36 @@
 var assert = require('assert');
 var layouts = require('nepalify').layouts;
 
+const input = ["+", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "=", ">", "?", "A", "B", "C",
+  "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+  "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+  "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"];
+
 describe('layouts.romanized', () => {
-  describe('formatKey()',() => {
+  describe('formatKey()', () => {
     it('Should return corresponding Nepali character.', () => {
-      const input = ["+", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "=", ">", "?", "A", "B", "C",
-                   "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
-                   "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a",  "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-                   "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"];
-                   
       const expected = ["\u200C", "।", "्", "०", "१", "२", "३", "४", "५", "६", "७", "८", "९", "ङ", "\u200D",
-         "॥", "?", "आ", "भ", "च", "ध", "ै","ऊ", "घ", "अ", "ी","झ", "ख", "ळ", "ं","ण", "ओ", "फ", "ठ", "ृ",
-         "श", "थ", "ू","ँ","औ", "ढ", "ञ", "ऋ", "इ", "ॐ", "ए", "^", " ","ऽ", "ा","ब", "छ", "द", "े", "उ",  
-         "ग",  "ह",  "ि", "ज",  "क",  "ल",  "म",  "न",  "ो", "प",  "ट", "र", "स", "त", "ु", "व",  "ौ", "ड",
-           "य",  "ष",  "ई",  "ः", "ऐ",  "़"];
-      
-       input.every((element, index, array) => {
+        "॥", "?", "आ", "भ", "च", "ध", "ै", "ऊ", "घ", "अ", "ी", "झ", "ख", "ळ", "ं", "ण", "ओ", "फ", "ठ", "ृ",
+        "श", "थ", "ू", "ँ", "औ", "ढ", "ञ", "ऋ", "इ", "ॐ", "ए", "^", " ", "ऽ", "ा", "ब", "छ", "द", "े", "उ",
+        "ग", "ह", "ि", "ज", "क", "ल", "म", "न", "ो", "प", "ट", "र", "स", "त", "ु", "व", "ौ", "ड",
+        "य", "ष", "ई", "ः", "ऐ", "़"];
+
+      input.every((element, index, array) => {
         assert.strictEqual(layouts.romanized.formatKey(element), expected[index]);
       });
     });
 
     it('Should return undefined if it fails to map', () => {
-      const inputMappedToNepali = ["+", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "=", ">", "?", "A", "B", "C",
-                   "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
-                   "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a",  "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-                   "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"];
-       for (var i=0; i<127; i++)
-       {
-         let input = String.fromCharCode(i);
-         found = inputMappedToNepali.find(element => element == input);
-         if(!found)
-         {
-           assert.strictEqual(layouts.romanized.formatKey(input), undefined);
-         }
-       }            
-       
+
+      for (var i = 0; i < 127; i++) {
+        let inputStr = String.fromCharCode(i);
+        found = input.find(element => element == inputStr);
+
+        if (!found) {
+          assert.strictEqual(layouts.romanized.formatKey(inputStr), undefined);
+        }
+      }
+
     });
   });
 });
