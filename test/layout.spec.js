4640ee0060cd6686aabfbfa3d7526bce44f575c6
Ansu Singh <ansumsingh@gmail.com>
2021-02-21 19:47:56 +1100

    Added undefined case as well

--- test/layout.spec.js
+++ test/layout.spec.js
@@ -19,5 +19,22 @@ describe('layouts.romanized', () => {
         assert.strictEqual(layouts.romanized.formatKey(element), expected[index]);
       });
     });
+
+    it('Should return undefined if it fails to map', () => {
+      const inputMappedToNepali = ["+", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "=", ">", "?", "A", "B", "C",
+                   "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+                   "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a",  "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                   "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"];
+       for (var i=0; i<127; i++)
+       {
+         let input = String.fromCharCode(i);
+         found = inputMappedToNepali.find(element => element == input);
+         if(!found)
+         {
+           assert.strictEqual(layouts.romanized.formatKey(input), undefined);
+         }
+       }            
+       
+    });
   });
 });
