032434374a084637cc968bee255ef31c8aaa38d5
Suvash Thapaliya <suvash@gmail.com>
2021-02-27 15:33:34 +0100

    Add test for layouts modules
    add chai to use expect style assertions

---
+++ test/layouts.spec.js
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import layouts from "../src/layouts";
+
+describe("layouts", () => {
+  it("should return an object of available layouts", () => {
+    expect(layouts).to.be.an('object');
+    expect(layouts).to.have.property('romanized');
+  });
+});
