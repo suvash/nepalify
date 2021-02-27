52a8a3c4fada38204162c6d17e3edc0863a2615f
Suvash Thapaliya <suvash@gmail.com>
2021-02-27 15:33:55 +0100

    Update romanized tests to use expect style assertions

--- test/layouts/romanized.spec.js
+++ test/layouts/romanized.spec.js
@@ -1,4 +1,4 @@
-import assert from "assert";
+import { expect } from "chai";
 import romanized from "../../src/layouts/romanized";
 import { testKeys } from "../helper.js";
 import { expectedKeys } from "./romanized.helper.js";
@@ -7,7 +7,7 @@ describe("romanized", () => {
   describe("formatKey()", () => {
     it("should return corresponding romanized Nepali character.", () => {
       const receivedKeys = testKeys.map((key) => romanized.formatKey(key));
-      assert.deepEqual(expectedKeys, receivedKeys);
+      expect(receivedKeys).to.deep.equal(expectedKeys);
     });
 
     it("should return undefined if no mapping exists", () => {
@@ -18,7 +18,7 @@ describe("romanized", () => {
       const receivedKeys = filteredKeys.map((key) => romanized.formatKey(key));
       const expectedKeys = [...Array(receivedKeys.length)];
 
-      assert.deepEqual(expectedKeys, receivedKeys);
+      expect(receivedKeys).to.deep.equal(expectedKeys);
     });
   });
 });
