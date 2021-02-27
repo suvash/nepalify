2df34d21e75d3426aaaaa642599b7dce76927659
Suvash Thapaliya <suvash@gmail.com>
2021-02-27 13:32:34 +0100

    Convert tests to use ESM style imports
    and extract common test input

--- test/layouts/romanized.spec.js
+++ test/layouts/romanized.spec.js
@@ -1,30 +1,26 @@
-var assert = require('assert');
-var romanized = require('../../src/layouts/romanized').default;
-
-const input = ["+", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "<", "=", ">", "?", "A", "B", "C",
-  "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
-  "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-  "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"];
+import assert from 'assert';
+import romanized from '../../src/layouts/romanized';
+import { testKeys } from '../helper.js';
 
 describe('romanized', () => {
   describe('formatKey()', () => {
-    it('Should return corresponding Nepali character.', () => {
+    it('should return corresponding Nepali character.', () => {
       const expected = ["\u200C", "।", "्", "०", "१", "२", "३", "४", "५", "६", "७", "८", "९", "ङ", "\u200D",
         "॥", "?", "आ", "भ", "च", "ध", "ै", "ऊ", "घ", "अ", "ी", "झ", "ख", "ळ", "ं", "ण", "ओ", "फ", "ठ", "ृ",
         "श", "थ", "ू", "ँ", "औ", "ढ", "ञ", "ऋ", "इ", "ॐ", "ए", "^", " ", "ऽ", "ा", "ब", "छ", "द", "े", "उ",
         "ग", "ह", "ि", "ज", "क", "ल", "म", "न", "ो", "प", "ट", "र", "स", "त", "ु", "व", "ौ", "ड",
         "य", "ष", "ई", "ः", "ऐ", "़"];
 
-      input.every((element, index, array) => {
+      testKeys.every((element, index, array) => {
         assert.strictEqual(romanized.formatKey(element), expected[index]);
       });
     });
 
-    it('Should return undefined if it fails to map', () => {
+    it('should return undefined if it fails to map', () => {
 
       for (var i = 0; i < 127; i++) {
         let inputStr = String.fromCharCode(i);
-        found = input.find(element => element == inputStr);
+        let found = testKeys.find(element => element == inputStr);
 
         if (!found) {
           assert.strictEqual(romanized.formatKey(inputStr), undefined);
