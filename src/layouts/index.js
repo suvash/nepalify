7a027dc2c41b3d7a50f7585e31e39cbe893c7e0a
Suvash Thapaliya <suvash@gmail.com>
2021-03-01 20:42:33 +0100

    Make 'traditional' layout available

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,9 @@
-import romanized from "./romanized/index.js";
+import romanized from "./romanized";
+import traditional from "./traditional";
 
 const layouts = {
   romanized: romanized,
+  traditional: traditional,
 };
 
 export default layouts;
--- test/layouts.spec.js
+++ test/layouts.spec.js
@@ -5,5 +5,6 @@ describe("layouts", () => {
   it("should return an object of available layouts", () => {
     expect(layouts).to.be.an("object");
     expect(layouts).to.have.property("romanized");
+    expect(layouts).to.have.property("traditional");
   });
 });
